beb75a126a0e68915b852b2e40c9346537c3f812
Silisors <gamerpg08@gmail.com>
2023-11-17 17:03:07 -0500

    añadiendo todos los archivos

---
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -0,0 +1,7 @@
+const serverIdPage = () => {
+    return ( 
+        <div>server ID page</div>
+     );
+}
+ 
+export default serverIdPage;
---
+++ lib/uploadthing.ts
@@ -0,0 +1,7 @@
+import { generateComponents } from "@uploadthing/react";
+ 
+import type { ourFileRouter } from "@/app/api/uploadthing/core";
+
+ 
+export const { UploadButton, UploadDropzone, Uploader } =
+  generateComponents<ourFileRouter>();
